2acba146d92fdb5a6ecee6bc95eb87142c872531
joey <doode1524@gmail.com>
2021-05-12 16:22:35 -0400

    app.js and history.js initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import history from "../history";
+// a react-router dependancy that allows you to manage session history through your routes without page refresh
+// dont forget to set up history.js inside src folder
+import { Router, Route, Switch } from "react-router-dom";
+// Use dynamic routing to handle routes in your app
+import { connect } from "react-redux";
+// connects react components to the store
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>     // Wrap Router in all components and add history as a prop
+      <div className="app">
+        <Switch>                   // Renders first child route that matches the location
+          <Route
+            exact                  // use exact path to ensure path matches
+            path="/pathname"       // name of path
+            render={(props) => (   // pass props to components
+              <NameOfComponent/>   // example component
+            )}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default App; // always remember to export your components
---
+++ src/history.js
@@ -0,0 +1,4 @@
+import { createBrowserHistory } from 'history'; 
+export default createBrowserHistory();
+
+// synchronizes our custom history to use with router
